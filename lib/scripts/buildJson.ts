dcac59c013a69fcf0c1b433faa4bcbd62c80e342
SSE World <suryakantmaurya2007@gmail.com>
2025-06-26 17:09:53 +0530

    feat: add buildJsonArray function to generate icons-array.json and update repository URLs in package.json

--- lib/scripts/buildJson.ts
+++ lib/scripts/buildJson.ts
@@ -28,6 +28,32 @@ async function buildJson() {
     console.log('icons.json has been generated successfully.');
 }
 
+async function buildJsonArray() {
+    const icons: IconDataJSONType[] = [];  // Change from object to array
+    for (const item of iconsData) {
+        const fileContent = await fs.readFileSync(path.join(svgDir, item.path), "utf-8");
+        icons.push({
+            id: item.id,
+            name: item.name,
+            path: `https://raw.githubusercontent.com/sse-icons/auth/main/${item.path}`,
+            body: fileContent,
+            version: item.version,
+            author: item.author,
+            categories: item.categories,
+            keywords: item.keywords,
+            url: item.url,
+        });
+    }
+    const jsonOutput = { icons };  // This remains the same
+    fs.writeFileSync(path.join(__dirname, '../../json/icons-array.json'), JSON.stringify(jsonOutput, null, 2));
+    console.log('icons.json has been generated successfully.');
+}
+
+
 buildJson().catch((error) => {
+    console.error("Error building JSON:", error);
+});
+
+buildJsonArray().catch((error) => {
     console.error("Error building JSON:", error);
 });
